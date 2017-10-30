4b455dd4cdbf4613ad490f0164393da205067e5a
Sarah Schaller <sarcsch@aol.com>
2017-10-30 20:32:30 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,10 +27,9 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 
-	private 
+	private
 
 	def post_params(*args)
 		params.require(:post).permit(*args)
 	end
 end
-
